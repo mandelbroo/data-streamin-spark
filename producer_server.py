fac65728086e74e0b9ae7f225df698877ad76a83
Vitalii <vitalii.makarets@gmail.com>
2020-03-08 13:22:32 +0200

    session 1

--- producer_server.py
+++ producer_server.py
@@ -2,9 +2,7 @@ from kafka import KafkaProducer
 import json
 import time
 
-
 class ProducerServer(KafkaProducer):
-
     def __init__(self, input_file, topic, **kwargs):
         super().__init__(**kwargs)
         self.input_file = input_file
@@ -16,10 +14,9 @@ class ProducerServer(KafkaProducer):
             for line in f:
                 message = self.dict_to_binary(line)
                 # TODO send the correct data
-                self.send()
+                self.send(topic=self.topic, value=message)
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode('utf-8')
